4a063784515dbf0fa21b3d5414510750ff6daa1e
Amr Elkfrawy <amrelkfrawy5@gmail.com>
2023-10-29 19:22:17 +0200

    Update .gitignore file

--- first.py
+++
@@ -1,6 +0,0 @@
-########################## FIRST ##########################
-# pip install djangorestframework
-
-########################## SECOND ##########################
-# pip install drf-nested-routers
-# pip install django-filter
