ad9e09cde68d44ca13d905447236e096357fb002
Amr Elkfrawy <amrelkfrawy5@gmail.com>
2023-11-08 12:37:39 +0200

    Add CORS middleware
    Allow cross-origin requests from 'http://localhost:8001'

--- storefront/settings.py
+++ storefront/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_filters',
+    'corsheaders',
     'rest_framework',
     'djoser',
     'playground',
@@ -58,6 +59,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -72,6 +74,11 @@ INTERNAL_IPS = [
     # ...
     '127.0.0.1',
     # ...
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8001',
+    'http://127.0.0.1:8001'
 ]
 
 ROOT_URLCONF = 'storefront.urls'
@@ -168,18 +175,16 @@ REST_FRAMEWORK = {
 }
 
 
-
 AUTH_USER_MODEL = 'core.User'
 
 DJOSER = {
-    'SERIALIZERS':{
-        'user_create':'core.serializers.UserCreateSerializer',
-        'current_user':'core.serializers.UserSerializer'
+    'SERIALIZERS': {
+        'user_create': 'core.serializers.UserCreateSerializer',
+        'current_user': 'core.serializers.UserSerializer'
     }
 }
 
 
-
 SIMPLE_JWT = {
-   'AUTH_HEADER_TYPES': ('JWT',),
+    'AUTH_HEADER_TYPES': ('JWT',),
     "ACCESS_TOKEN_LIFETIME": timedelta(days=1)}
